de42d8ee4fa536aebbec6082d0a2506403cd64bc
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-10 19:50:39 -0400

    add doneish

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,58 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    public Node getNode(Integer index){
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.next;
+    }
+
+    public void addNode(Integer data){
+        Node node = new Node(data);
+        if (head == null){
+            head = node;
+        }
+        else {
+            Node temp = head;
+            if (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+
+    }
+
+    public void removeAtIndex(){
+
+    }
+
+    public Boolean contains(){
+        return null;
+    }
+
+    public Integer find(){
+        return null;
+    }
+
+    public Integer size(){
+        return null;
+    }
+
+    public Node copy(){
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList testList = new SinglyLinkedList();
+
+    @Before
+    public void setup(){
+    }
+    @Test
+    public void addTest(){
+        testList.addNode(13);
+        testList.addNode(24);
+        testList.addNode(35);
+
+    }
+
 }

f12fe04bcb9e36fb947600995c71f8aca7530a05
gdavis <jrobley3@hotmail.com>
2020-03-29 17:56:21 -0400

    work please

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -4,21 +4,65 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
-    SinglyLinkedList testList = new SinglyLinkedList();
+    SinglyLinkedList sl = new SinglyLinkedList();
 
     @Before
-    public void setup(){
+    public void init(){
+        sl.add("I");
+        sl.add("am");
+        sl.add("Iron Man");
     }
+
+
+
     @Test
-    public void addTest(){
-        testList.addNode(13);
-        testList.addNode(24);
-        testList.addNode(35);
-
+    public void removeAtTest(){
+        sl.removeAtIndex(1);
+        int expected = 11;
+        int actual = sl.find(1);
+        Assert.assertEquals(expected,actual);
     }
 
+    @Test
+    public void containsTest(){
+        Assert.assertTrue( sl.contains(11));
+    }
+
+    @Test
+    public void findTest(){
+        sl.find(1);
+        int expected = 2;
+        int actual = sl.find(1);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void sizeTest(){
+        int expected = 3;
+        int actual = sl.size();
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void copyTest(){
+        SinglyLinkedList sl2 = SinglyLinkedList.copy(sl);
+        Assert.assertFalse(sl2==sl);
+    }
+
+//    @Test
+//    public void sortTest(){
+//
+//        sl.sort();
+//
+//        String expected = "2\n5\n11";
+//        String actual = sl.printAllNodes();
+//        Assert.assertEquals(expected,actual);
+//    }
 }
